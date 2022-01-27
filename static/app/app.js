49db40e4dd6edb35ba34ea73d081082c489cdae8
therealtgd <vladanmikic2000@gmail.com>
2022-01-27 20:08:24 +0100

    Adds router links

--- static/app/app.js
+++ static/app/app.js
@@ -1,13 +1,17 @@
-const Login = { template: '<login></login>' }
+const LoginForm = { template: '<login></login>' }
+const RegisterForm = { template: '<register></register>' }
 
 const router = new VueRouter({
     mode: 'hash',
     routes: [
-        { path: '/', component: Login },
+        { path: '/', component: LoginForm },
+        { path: '/login/', component: LoginForm },
+        { path: '/register/', component: RegisterForm },
+        // { path: '/login/forgot', component: RegisterForm },
     ]
 });
 
 var app = new Vue({
     router,
-    el: '#login'
+    el: '#wrapper'
 });
