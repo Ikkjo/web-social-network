bdd23552ee136183b99c532c5b7eaac227073f86
therealtgd <vladanmikic2000@gmail.com>
2022-01-28 20:18:48 +0100

    Adds validators to login

--- static/app/login.js
+++ static/app/login.js
@@ -1,12 +1,14 @@
 Vue.component("login", {
     data() {
         return {
-            valid: false,
-            showPassword: false,
-            userInfo: {
+            form: {
                 username: '',
                 password: '',
             },
+            infocus: {
+                username: true,
+                password: true,
+            }
         }
     },
     props: ["submitForm"],
@@ -16,20 +18,26 @@ Vue.component("login", {
             <div class="form-container">
                 <form class="login-form">
                     <h2 class="title">Uloguj se</h2>
-                    <div class="input-field">
-                    <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Korisničko ime" name="username"" required/>
+                    <div class="form-element">
+                        <div class="input-field">
+                            <i class="fas fa-user"></i>
+                            <input v-model="form.username" @focus="inFocus('username')" @blur="outFocus('username')" type="text" placeholder="Korisničko ime" name="username"" pattern="^[a-z0-9_-]{3,15}$"/>
+                        </div>
+                        <div v-show="!isFocused('username') && $v.form.username.$invalid" class="alert alert-danger">Korisničko ime je obavezno.</div>
                     </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Lozinka" name="password"  required/>
+                    <div class="form-element">
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input v-model="form.password" @focus="inFocus('password')" @blur="outFocus('password')" type="password" placeholder="Lozinka" name="password"/>
+                        </div>
+                        <div v-show="!isFocused('password') && $v.form.password.$invalid" class="alert alert-danger">Lozinka je obavezna.</div>
                     </div>
                     <div class="links">
                         <router-link to="/login/forgot">Zaboravljena lozinka?</router-link>
                         <router-link to="/register">Registruj se</router-link>
                     </div>
                     <div class="buttons-div">
-                        <input type="submit" value="Uloguj se" class="btn solid" />
+                        <input :disabled="$v.form.$invalid" type="submit" value="Uloguj se" class="btn solid" />
                         <button class="btn transparent">Nastavi kao gost</button>
                     </div>
                 </form>
@@ -40,8 +48,28 @@ Vue.component("login", {
         </div>
     </div>	 
 `,
-    methods: {},
+    methods: {
+        isFocused(field) {
+            return this.infocus[field]
+        },
+        inFocus(field) {
+            this.infocus[field] = true
+        },
+        outFocus(field) {
+            this.infocus[field] = false
+        }
+    },
     mounted() {},
+    validations: {
+        form: {
+            username: {
+                required: validators.required,
+            },
+            password: {
+                required: validators.required,
+            },
+        }
+    }
 });
 
 // new Vue({}).$mount("#wrapper")
