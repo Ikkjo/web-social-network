9af49a00a4b11352d934ecf32f4ffa9a901ee80e
therealtgd <vladanmikic2000@gmail.com>
2022-03-16 21:28:18 +0100

    Fixes AuthController

--- src/controllers/AuthController.java
+++ src/controllers/AuthController.java
@@ -2,15 +2,48 @@ package controllers;
 
 import beans.models.User;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import services.UserService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 import static spark.Spark.post;
 
 public class AuthController {
 
-    public  AuthController(final UserService userService) {
+    private static UserService userService;
 
-        post("/login", (request, response) -> {
+    public AuthController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    public static Route login = (Request request, Response response) -> {
+        response.type("application/json");
+        User user = new Gson().fromJson(request.body(), User.class);
+        if(userService.isValidUser(user)) {
+            user = userService.getUser(user.getUsername());
+            // create login token
+            return new Gson().toJson(user);
+        }
+        response.status(401);
+        return response;
+    };
+
+    public static Route register = (Request request, Response response) -> {
+        response.type("application/json");
+        User user = new Gson().fromJson(request.body(), User.class);
+        if (userService.isValidUsername(user.getUsername()) && userService.isValidEmail(user.getEmail())) {
+            userService.addUser(user);
+            return new Gson().toJson(user);
+        }
+        response.status(401);
+        return response;
+    };
+
+    public static void run(final UserService userService) {
+
+        post("/login/", (request, response) -> {
             response.type("application/json");
             User user = new Gson().fromJson(request.body(), User.class);
             if(userService.isValidUser(user)) {
@@ -22,7 +55,7 @@ public class AuthController {
             return response;
         });
 
-        post("/registration", (request, response) -> {
+        post("/register/", (request, response) -> {
             response.type("application/json");
             User user = new Gson().fromJson(request.body(), User.class);
             if (userService.isValidUsername(user.getUsername()) && userService.isValidEmail(user.getEmail())) {
