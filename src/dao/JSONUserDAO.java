e1fb0552bc7837e15a6b0372b62d3514384c1272
therealtgd <vladanmikic2000@gmail.com>
2022-03-18 22:17:23 +0100

    Implements user search functions

--- src/dao/JSONUserDAO.java
+++ src/dao/JSONUserDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import beans.models.Gender;
 import beans.models.User;
 
 import java.util.ArrayList;
@@ -38,5 +39,39 @@ public class JSONUserDAO implements UserDAO{
             }
         }
         return null;
+    }
+
+    @Override
+    public HashMap<String, User> getUsersByName(String name) {
+        HashMap<String, User> usersMap = new HashMap<>();
+        for (User u: users.values()) {
+            if (u.getName().equals(name))
+                usersMap.put(u.getUsername(), u);
+        }
+        return usersMap;
+    }
+
+    @Override
+    public HashMap<String, User> getUsersBySurname(String surname) {
+        HashMap<String, User> usersMap = new HashMap<>();
+        for (User u: users.values()) {
+            if (u.getSurname().equals(surname))
+                usersMap.put(u.getUsername(), u);
+        }
+        return usersMap;
+    }
+
+    @Override
+    public HashMap<String, User> getUsersByDateRange(String dateRange) {
+        return null;
+    }
+
+    public void generateTestData() {
+        User u1 = new User("test1", "test1", "test1@gmail.com", "Test1", "Testic1", Gender.MALE);
+        User u2 = new User("test2", "test2", "test2@gmail.com", "Test2", "Testic2", Gender.FEMALE);
+        User u3 = new User("test3", "test3", "test3@gmail.com", "Test3", "Testic1", Gender.FEMALE);
+        users.put(u1.getUsername(), u1);
+        users.put(u2.getUsername(), u2);
+        users.put(u3.getUsername(), u3);
     }
 }
