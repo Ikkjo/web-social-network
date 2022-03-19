072082a6aa2a1d8b8b73ef11d74bc481cee90823
therealtgd <vladanmikic2000@gmail.com>
2022-03-19 22:03:59 +0100

    Removes > from buttons

--- static/app/userSearchResult.js
+++ static/app/userSearchResult.js
@@ -8,8 +8,8 @@ Vue.component("user-search-result", {
     template: `
     <div class="container user-search-result-container">
         <user-thumbnail :user="user">01.01.2000.</user-thumbnail>
-        <button v-if="!isFriend" @click="sendFriendRequest" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>>
-        <button v-if="isFriend" @click="sendMessage" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>>
+        <button v-if="!isFriend" @click="sendFriendRequest" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>
+        <button v-if="isFriend" @click="sendMessage" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>
     </div>
     `,
     methods: {
