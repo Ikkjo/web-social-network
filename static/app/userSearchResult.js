ac530cbe3a5c591f7e67459e3c7986f74da269f9
therealtgd <vladanmikic2000@gmail.com>
2022-03-20 00:49:39 +0100

    Fixes button text in userSearchResult

--- static/app/userSearchResult.js
+++ static/app/userSearchResult.js
@@ -2,14 +2,14 @@ Vue.component("user-search-result", {
     props: ["user"],
     data() {
         return {
-            isFriend: true,
+            isFriend: false,
         }
     },
     template: `
     <div class="container user-search-result-container">
         <user-thumbnail :user="user"></user-thumbnail>
-        <button v-if="!isFriend" @click="sendFriendRequest" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>
-        <button v-if="isFriend" @click="sendMessage" class="btn"><i class="fas fa-sign-out-alt"></i> Odjavi se</button></a>
+        <button v-if="!isFriend" @click="sendFriendRequest" class="btn"><i class="fas fa-user-plus"></i> Pošalji zahtev</button></a>
+        <button v-if="isFriend" @click="sendMessage" class="btn"><i class="fas fa-comment-dots"></i> Poruka</button></a>
     </div>
     `,
     methods: {
