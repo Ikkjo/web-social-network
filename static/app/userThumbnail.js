f3230ecd5143f61ec7392dbe719e7100739f3771
therealtgd <vladanmikic2000@gmail.com>
2022-03-19 21:43:27 +0100

    Adds userThumbnail.js

---
+++ static/app/userThumbnail.js
@@ -0,0 +1,23 @@
+Vue.component("user-thumbnail", {
+    props: ["user"],
+    data() {
+        return {}
+    },
+    template: `
+    <div class="container user-thumbnail-container">
+        <img :src="user.profilePic" alt="Profilna slika"></img>
+        {{user.profilePic}}
+        <h2>{{user.name}}</h2>
+        <h2>{{user.surname}}</h2>
+    </div>
+    `,
+    methods: {
+        sendFriendRequest() {
+            console.log("Friend request sent...");
+        },
+        sendMessage() {
+            console.log("Sending message...");
+        },
+    },
+    mounted() {},
+});
