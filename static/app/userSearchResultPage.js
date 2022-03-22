388407a3ecfc3c05e3736fdfa6e4bab2466bc1d3
therealtgd <vladanmikic2000@gmail.com>
2022-03-22 18:30:42 +0100

    Adds sort select

--- static/app/userSearchResultPage.js
+++ static/app/userSearchResultPage.js
@@ -2,6 +2,7 @@ Vue.component("user-search-result-page", {
     data() {
         return {
             users: null,
+            params: null,
         }
     },
     template: `
@@ -9,6 +10,14 @@ Vue.component("user-search-result-page", {
         <nav-bar></nav-bar>
         <div class="user-search-result-page-container">
             <div class="user-search-inner-container">
+                <div class="sort-continer">
+                    <label for="sort">Kriterijum sortiranja:</label>
+                    <select name="sort" id="sort">
+                        <option v-if="this.params.name" value="name">Ime</option>
+                        <option v-if="this.params.surname" value="surname">Prezime</option>
+                        <option v-if="this.params.date" value="date">Datum rođenja</option>
+                    </select>
+                </div>
                 <user-search-result v-for="u in users" :user="u" :key="u.username"></user-search-result>            
             </div>
         </div>
@@ -16,9 +25,9 @@ Vue.component("user-search-result-page", {
     `,
     methods: {},
     mounted() {
-        let params = this.$route.params;
+        this.params = this.$route.params;
         axios.get("/user-search/", {
-            params: params
+            params: this.params
         }).then(response => this.users = response.data)
         .catch(error => alert("Pretraga neuspješna."));
     },
