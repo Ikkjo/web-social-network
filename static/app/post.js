24a354897f491aa301b53019be8ddb7b635db0ac
therealtgd <vladanmikic2000@gmail.com>
2022-09-02 18:54:22 +0200

    Finished post detailed view

--- static/app/post.js
+++ static/app/post.js
@@ -8,10 +8,11 @@ Vue.component("post", {
                     profilePic: "../img/female_avatar.svg",
                     username: "nekoime"
                 },
-                type: 'photo',
+                type: 'text',
                 photo: "../img/avatar1.jpg",
                 text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repellendus enim doloremque rem doloribus commodi fugit nam quisquam sequi corporis?'
             },
+            detailedView: false,
             user: {
                 username: "nekoime"
             }
@@ -20,13 +21,26 @@ Vue.component("post", {
     template: ` 
     <div id="post">
         <div class="post-container">
-            <i v-if="post.user.username === user.username" class="fas fa-trash-alt delete-post"></i>
-            <img v-if="post.type==='photo'" class="image-div picture-container" :src="post.photo" alt="" srcset="">
-            <div class="post-content">
+            <i v-if="!detailedView" @click="detailedView=!detailedView" class="fas fa-arrow-down post-toggle"></i>
+            <i v-if="detailedView" @click="detailedView=!detailedView" class="fas fa-arrow-up post-toggle"></i>
+            <i v-if="post.user.username === user.username && detailedView" class="fas fa-trash-alt delete-post"></i>
+            
+            <div v-if="!detailedView">
+                <user-thumbnail
+                        :user="post.user"
+                        :useDate="false" 
+                        class="user-thumbnail user-thumbnail-collapsed"/>
+                <img v-if="post.type==='photo'" class="image-div picture-container" :src="post.photo" alt="" srcset="">
+                <div v-if="post.type==='text'" class="text-div text-div-collapsed">{{post.text}}</div>
+            </div>
+
+            <img v-if="post.type==='photo' && detailedView" class="image-div picture-container" :src="post.photo" alt="" srcset="">
+            <div v-if="detailedView" class="post-content">
                 <user-thumbnail
                     :user="post.user"
                     :useDate="false" 
-                    class="user-thumbnail"/>
+                    class="user-thumbnail"
+                    :class="{'user-thumbnail-text': (detailedView && post.type==='text')}"/>
                 <div class="text-div">{{post.text}}</div>
                 <img v-if="post.type==='text' && post.photo" class="image-div picture-container" :src="post.photo" alt="" srcset="">
                 <div class="comments-div">
